f02c05e3741afe4b1b014dcb1831dfed78edf5f1
Filype Abreu <filype.abreu@gmail.com>
2025-02-11 02:14:51 -0300

    feat: home page fianlizada

--- script/home.js
+++ script/home.js
@@ -10,7 +10,7 @@ let lobo1 = lista[randint(tamanho)];
 
 document.getElementById("img_lobo1").src = lobo1.imagem;
 document.getElementById("nome_lobo1").innerText = lobo1.nome;
-document.getElementById("id_lobo1").innerText = lobo1.id;
+document.getElementById("id_lobo1").innerText = `Idade: ${lobo1.idade}`;
 document.getElementById("descricao_lobo1").innerText = lobo1.descricao;
 
 do {
@@ -19,5 +19,5 @@ do {
 
 document.getElementById("img_lobo2").src = lobo2.imagem;
 document.getElementById("nome_lobo2").innerText = lobo2.nome;
-document.getElementById("id_lobo2").innerText = lobo2.id;
+document.getElementById("id_lobo2").innerText = `Idade: ${lobo2.idade}`;
 document.getElementById("descricao_lobo2").innerText = lobo2.descricao;
