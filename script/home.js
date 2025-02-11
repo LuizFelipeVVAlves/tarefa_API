22764adb530fa26df0cc10d67b8052066ad4ee20
Filype Abreu <filype.abreu@gmail.com>
2025-02-11 01:24:10 -0300

    feat: adiciona lobos aleatorios

---
+++ script/home.js
@@ -0,0 +1,23 @@
+let lista = JSON.parse(localStorage.getItem('lobos'));
+
+let tamanho = lista.length;
+
+function randint(tamanho) {
+    return Math.floor(Math.random() * (tamanho + 1));
+}
+
+let lobo1 = lista[randint(tamanho)];
+
+document.getElementById("img_lobo1").src = lobo1.imagem;
+document.getElementById("nome_lobo1").innerText = lobo1.nome;
+document.getElementById("id_lobo1").innerText = lobo1.id;
+document.getElementById("descricao_lobo1").innerText = lobo1.descricao;
+
+do {
+    lobo2 = lista[randint(tamanho)];
+} while (lobo1 == lobo2);
+
+document.getElementById("img_lobo2").src = lobo2.imagem;
+document.getElementById("nome_lobo2").innerText = lobo2.nome;
+document.getElementById("id_lobo2").innerText = lobo2.id;
+document.getElementById("descricao_lobo2").innerText = lobo2.descricao;
