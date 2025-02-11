627d781d6457c677d1e0b4f4de9160f1abc25326
Filype Abreu <filype.abreu@gmail.com>
2025-02-10 23:29:06 -0300

    feat: adotar lobinho adicionado

--- script/adotar.js
+++ script/adotar.js
@@ -9,9 +9,36 @@ let lobo = lista.find((lobo)=>{
 let nome = lobo.nome;
 let photo = lobo.imagem;
 
-
 document.getElementById("nomeDoLobo").innerText = nome;
 document.getElementById("idDoLobo").innerText = id;
 document.getElementById("img__lobo").src = photo;
+
+let button = document.getElementById('botao__adotar');
+
+button.addEventListener("click", ()=> {
+    let nameOwner = document.getElementById("name").value;
+    let ageOwner = parseInt(document.getElementById("age").value);
+    let emailOwner = document.getElementById("email").value;
+
+    lobinhoAdotar(nameOwner, ageOwner, emailOwner);
+})
+
+function lobinhoAdotar(nameOwner, ageOwner, emailOwner) {
+    try {
+        let index = lista.indexOf(lobo);
+        console.log(index);
+        lista[index].adotado = true;
+        lista[index].nomeDono = nameOwner;
+        lista[index].idadeDono = ageOwner;
+        lista[index].emailDono = emailOwner;
+        localStorage.setItem('lobos', JSON.stringify(lista));
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
 
 
