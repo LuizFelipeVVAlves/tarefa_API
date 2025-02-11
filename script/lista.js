db8c7e51eb5fca04224134fb3c9ff1ea1619a95c
Filype Abreu <filype.abreu@gmail.com>
2025-02-11 18:46:16 -0300

    feat: prototipo lista lobinho

---
+++ script/lista.js
@@ -0,0 +1,25 @@
+let lista = JSON.parse(localStorage.getItem('lobos'));
+
+const itensPorPagina = 4;
+let pagina = 1;
+let lado = true;
+
+function gerarLista() {
+    const lobosPaginados = lista.slice((pagina - 1) * itensPorPagina, pagina * itensPorPagina);
+    
+
+}
+
+function gerarPaginacao() {
+    const maximoPaginas = Math.ceil(lista.length/itensPorPagina);
+    for(let i = 1; i <= maximoPaginas; i++) {
+        
+    }
+}
+
+
+
+
+
+
+
